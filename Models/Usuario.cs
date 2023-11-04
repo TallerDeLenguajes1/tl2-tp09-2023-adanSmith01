1ddaadd1e6d6abdd858e24f297eaf839e0b8dc6e
adanSmith01 <smith.paredes.13@gmail.com>
2023-11-04 18:25:12 -0300

    Agrego los modelos para el proyecto

---
+++ Models/Usuario.cs
@@ -0,0 +1,10 @@
+namespace tl2_tp09_2023_adanSmith01;
+
+public class Usuario
+{
+    private int id;
+    private string nombreUsuario;
+
+    public int Id { get => id; set => id = value; }
+    public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+}
