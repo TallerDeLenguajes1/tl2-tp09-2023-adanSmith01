2eeb24f4c45a9b888e240fe33306437d94e2e25a
adanSmith01 <smith.paredes.13@gmail.com>
2023-11-05 11:45:05 -0300

    Agrego la interfaz del repositorio de tablero

---
+++ Repository/ITableroRepository.cs
@@ -0,0 +1,11 @@
+namespace tl2_tp09_2023_adanSmith01;
+
+public interface ITableroRepository
+{
+    void CrearTablero(Tablero nuevoTablero);
+    void ModificarTablero(int idTablero, Tablero tableroModificar);
+    List<Tablero> GetAllTableros();
+    Tablero GetTableroPorId(int idTablero);
+    List<Tablero> GetTablerosDeUsuario(int idUsuario);
+    void EliminarTablero(int idTablero);
+}
