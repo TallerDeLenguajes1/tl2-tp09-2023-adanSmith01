5c1f1ba9e09ddacf1672d31c20d48c968c17da4c
adanSmith01 <smith.paredes.13@gmail.com>
2023-11-05 16:58:30 -0300

    Cambio de nombre del endpoint 'CrearTablero' por 'Crear'

--- Controllers/TableroController.cs
+++ Controllers/TableroController.cs
@@ -6,7 +6,7 @@ namespace tl2_tp09_2023_adanSmith01.Controllers;
 public class TableroController : ControllerBase
 {
     private readonly ILogger<TableroController> _logger;
-    private TableroRepository tableroRepo;
+    private ITableroRepository tableroRepo;
     public TableroController(ILogger<TableroController> logger)
     {
         _logger = logger;
@@ -25,32 +25,13 @@ public class TableroController : ControllerBase
     }
 
     [HttpPost("/api/tablero")]
-    public ActionResult<string> CrearTablero(Tablero nuevoTablero){
+    public ActionResult<string> Crear(Tablero nuevoTablero){
         if(nuevoTablero != null){
             tableroRepo.CrearTablero(nuevoTablero);
             return Ok("Se ha creado un nuevo tablero correctamente");
         } else{
             return BadRequest("ERROR. No se ha creado el tablero correctamente");
         }
-    }
-
-    //Probando el GET y DELETE faltantes
-    [HttpGet]
-    [Route("/api/tablero/{idUsuario}")]
-    public ActionResult<IEnumerable<Tablero>> GetTablerosPorIdUsuario(int idUsuario){
-        var tablerosDeUsuario = tableroRepo.GetTablerosDeUsuario(idUsuario);
-
-        if(tablerosDeUsuario.Count != 0){
-            return Ok(tablerosDeUsuario);
-        } else{
-            return BadRequest("No hay tableros registrados del usuario indicado");
-        }
-    }
-
-    [HttpDelete("/api/tablero/{id}")]
-    public ActionResult<string> BorrarTablero(int id){
-        tableroRepo.EliminarTablero(id);
-        return Ok("La eliminacion del tablero se realizo correctamente");
     }
     
 }
